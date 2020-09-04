7e2471cdd657afc49fd210d04d5c51a2eaff6f98
Vivek Sanghvi <viksipart@gmail.com>
2020-09-04 18:23:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if (board[position] ==  " " || board[position] == "" || board[position] == nil)
-     return false
-   end
+  #   return false
 
 
 
