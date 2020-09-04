0e4cdd45dca1f1f094f32a2ba7c7d8df6f16adff
Vivek Sanghvi <viksipart@gmail.com>
2020-09-04 18:14:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board = " " || index = " "
-    false
-  elsif (board = "X" || board = "O")
+    return false
+  elsif (index = "X" || index = "O")
     return true 
   end
 end
